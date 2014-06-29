d1d85aa7cb584dcfddf4ce572abf5ef8c81ba7ca
deeeki <deeeki@gmail.com>
2014-06-29 11:13:14 +0900

    Report the coverage to codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ if ENV['CI'] && RUBY_VERSION.start_with?('2.1')
   require 'coveralls'
   Coveralls.wear!('test_frameworks')
 end
+if ENV['CODECLIMATE_REPO_TOKEN'] && RUBY_VERSION.start_with?('2.1')
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 
 require 'minitest/autorun'
 require 'minitest/pride'
